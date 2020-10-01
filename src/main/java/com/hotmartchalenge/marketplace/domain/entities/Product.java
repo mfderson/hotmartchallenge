2b80f1bfa9189180b49d7f87db3faf7792e08390
Anderson Freitas <anderson.freitas@engdb.com.br>
2020-10-01 19:54:58 -0300

    feat(domain): entities
    criada entidade de produto com os atributos basicos

---
+++ src/main/java/com/hotmartchalenge/marketplace/domain/entities/Product.java
@@ -0,0 +1,28 @@
+package com.hotmartchalenge.marketplace.domain.entities;
+
+import java.time.OffsetDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Product {
+  
+  @Id
+  @EqualsAndHashCode.Include
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String name;
+
+  private String description;
+
+  private OffsetDateTime createdAt;
+}
