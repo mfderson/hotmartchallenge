778c8dd13cb03b9810977f892ea814b47f16c03a
Anderson Freitas <anderson.freitas@engdb.com.br>
2020-10-01 21:55:28 -0300

    feat(repositorio): produto
    adicionado repositorio para produto

---
+++ src/main/java/com/hotmartchalenge/marketplace/domain/repositories/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.hotmartchalenge.marketplace.domain.repositories;
+
+import com.hotmartchalenge.marketplace.domain.entities.Product;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+  
+}
