e31484cadec1e42af8dc4f0d01c2077177a6db0d
Anderson Freitas <anderson.freitas@engdb.com.br>
2020-10-02 21:43:33 -0300

    feat(dto): product
    adicionada classe com os dto's de response e request

---
+++ src/main/java/com/hotmartchalenge/marketplace/api/dtos/request/ProductReqDto.java
@@ -0,0 +1,17 @@
+package com.hotmartchalenge.marketplace.api.dtos.request;
+
+import javax.validation.constraints.NotBlank;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ProductReqDto {
+  
+  @NotBlank
+  private String name;
+
+  @NotBlank
+  private String description;
+}
---
+++ src/main/java/com/hotmartchalenge/marketplace/api/dtos/response/ProductResDto.java
@@ -0,0 +1,15 @@
+package com.hotmartchalenge.marketplace.api.dtos.response;
+
+import java.time.OffsetDateTime;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ProductResDto {
+  private Long id;
+  private String name;
+  private String description;
+  private OffsetDateTime createdAt;
+}
