7a17db0bcde7438f4e40331f1c28dcd393dbf050
Anderson Freitas <mfderson@gmail.com>
2020-10-03 14:08:25 -0300

    feat(api): controller
    disponibilizado endpoint para inserção de um novo produto. Já usando o padrão dto

--- src/main/java/com/hotmartchalenge/marketplace/api/controllers/ProductController.java
+++ src/main/java/com/hotmartchalenge/marketplace/api/controllers/ProductController.java
@@ -1,13 +1,20 @@
 package com.hotmartchalenge.marketplace.api.controllers;
 
+import com.hotmartchalenge.marketplace.api.assemblers.ProductReqDtoDisassembler;
 import com.hotmartchalenge.marketplace.api.assemblers.ProductResDtoAssembler;
+import com.hotmartchalenge.marketplace.api.dtos.request.ProductReqDto;
 import com.hotmartchalenge.marketplace.api.dtos.response.ProductResDto;
 import com.hotmartchalenge.marketplace.domain.entities.Product;
 import com.hotmartchalenge.marketplace.domain.services.ProductService;
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,10 +24,20 @@ public class ProductController {
 
   @Autowired private ProductResDtoAssembler productResDtoAssembler;
 
+  @Autowired private ProductReqDtoDisassembler productReqDtoDisassembler;
+
   @GetMapping("/{id}")
   public ProductResDto findById(@PathVariable Long id) {
     Product product = productService.findById(id);
 
     return productResDtoAssembler.toDto(product);
+  }
+
+  @PostMapping
+  @ResponseStatus(HttpStatus.CREATED)
+  public ProductResDto insert(@RequestBody @Valid ProductReqDto productReqDto) {
+    Product product = productReqDtoDisassembler.toEntityObject(productReqDto);
+
+    return productResDtoAssembler.toDto(productService.save(product));
   }
 }
