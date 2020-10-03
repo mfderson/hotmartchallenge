76c92750e932b8a01e8ea7dc307b51df0e8982e9
Anderson Freitas <anderson.freitas@engdb.com.br>
2020-10-02 21:45:32 -0300

    feat(service): product
    adicionado método findById na classe de serviço

---
+++ src/main/java/com/hotmartchalenge/marketplace/domain/services/ProductService.java
@@ -0,0 +1,19 @@
+package com.hotmartchalenge.marketplace.domain.services;
+
+import com.hotmartchalenge.marketplace.domain.entities.Product;
+import com.hotmartchalenge.marketplace.domain.exceptions.ProductNotFoundException;
+import com.hotmartchalenge.marketplace.domain.repositories.ProductRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+  @Autowired
+  private ProductRepository productRepository;
+
+  public Product findById(Long id) {
+    return productRepository.findById(id).orElseThrow(
+      () -> new ProductNotFoundException(id));
+  }
+}
