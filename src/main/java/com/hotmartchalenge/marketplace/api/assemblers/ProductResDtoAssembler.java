d830f6866bb15869649ad5d1af772cbe954b1f69
Anderson Freitas <anderson.freitas@engdb.com.br>
2020-10-02 21:42:26 -0300

    feat(assemblers): product
    adicionado classes para fazer o assemble de entidade para dto e o disassemble de dto para entidade

---
+++ src/main/java/com/hotmartchalenge/marketplace/api/assemblers/ProductResDtoAssembler.java
@@ -0,0 +1,27 @@
+package com.hotmartchalenge.marketplace.api.assemblers;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.hotmartchalenge.marketplace.api.dtos.response.ProductResDto;
+import com.hotmartchalenge.marketplace.domain.entities.Product;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProductResDtoAssembler {
+  @Autowired
+  private ModelMapper modelMapper;
+
+  public ProductResDto toDto(Product product) {
+    return modelMapper.map(product, ProductResDto.class);
+  }
+
+  public List<ProductResDto> toCollectionDto(List<Product> products) {
+    return products.stream()
+      .map(product -> toDto(product))
+      .collect(Collectors.toList());
+  }
+}
