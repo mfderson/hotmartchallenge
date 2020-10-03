3429037fb15dd3d23fd17c8232b1e6c78468a6e5
Anderson Freitas <mfderson@gmail.com>
2020-10-03 16:56:20 -0300

    feat(domain): entity
    adicionado relacionamento OneToMany de sales na tabela product. Implementada regra de negócio para retornar a média da pontuação de vendas nos últimos 12 meses e também a regra pra calcular a proporção de vendas por dia do produto.

--- src/main/java/com/hotmartchalenge/marketplace/domain/entities/Product.java
+++ src/main/java/com/hotmartchalenge/marketplace/domain/entities/Product.java
@@ -1,12 +1,16 @@
 package com.hotmartchalenge.marketplace.domain.entities;
 
 import java.time.OffsetDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import org.hibernate.annotations.CreationTimestamp;
@@ -30,4 +34,28 @@ public class Product {
   @ManyToOne
   @JoinColumn(name = "category_id")
   private Category category;
+
+  @OneToMany(mappedBy = "product")
+  private List<Sale> sales = new ArrayList<>();
+
+  public double getAverageLastTwelveMonths() {
+    double sumRating = 0.0;
+    OffsetDateTime datetimeTwelveMonthsAgo = OffsetDateTime.now().minusMonths(12L);
+    int numberOfSales = 0;
+
+    for (Sale sale : sales) {
+      if (datetimeTwelveMonthsAgo.compareTo(sale.getCreatedAt()) >= 0) {
+        numberOfSales++;
+        sumRating += sale.getRating();
+      }
+    }
+
+    return numberOfSales > 0 ? sumRating / numberOfSales : 0;
+  }
+
+  public double getSalesPerDay() {
+    Long numberOfDays = ChronoUnit.DAYS.between(OffsetDateTime.now(), getCreatedAt());
+
+    return numberOfDays > 0 ? getSales().size() / numberOfDays : 0;
+  }
 }
