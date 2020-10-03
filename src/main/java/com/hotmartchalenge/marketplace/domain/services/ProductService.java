1295222f8af5bf355a0732c538f8993eadcf7fb6
Anderson Freitas <mfderson@gmail.com>
2020-10-03 14:04:20 -0300

    feat(domain): service
    implementação do método para inserir um novo produto

--- src/main/java/com/hotmartchalenge/marketplace/domain/services/ProductService.java
+++ src/main/java/com/hotmartchalenge/marketplace/domain/services/ProductService.java
@@ -1,16 +1,34 @@
 package com.hotmartchalenge.marketplace.domain.services;
 
+import com.hotmartchalenge.marketplace.domain.entities.Category;
 import com.hotmartchalenge.marketplace.domain.entities.Product;
+import com.hotmartchalenge.marketplace.domain.exceptions.CategoryNotFoundException;
 import com.hotmartchalenge.marketplace.domain.exceptions.ProductNotFoundException;
+import com.hotmartchalenge.marketplace.domain.repositories.CategoryRepository;
 import com.hotmartchalenge.marketplace.domain.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class ProductService {
   @Autowired private ProductRepository productRepository;
 
+  @Autowired private CategoryRepository categoryRepository;
+
   public Product findById(Long id) {
     return productRepository.findById(id).orElseThrow(() -> new ProductNotFoundException(id));
+  }
+
+  @Transactional
+  public Product save(Product product) {
+    Category category = findCategoryById(product.getCategory().getId());
+    product.setCategory(category);
+
+    return productRepository.save(product);
+  }
+
+  private Category findCategoryById(Long id) {
+    return categoryRepository.findById(id).orElseThrow(() -> new CategoryNotFoundException(id));
   }
 }
