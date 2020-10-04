f5e3c98e62cdcf571ce0fd12b764c2c8a6a1575b
Anderson Freitas <mfderson@gmail.com>
2020-10-04 09:49:37 -0300

    feat(domain): repository
    extendendo o repositório de customRepository

--- src/main/java/com/hotmartchalenge/marketplace/domain/repositories/ProductRepository.java
+++ src/main/java/com/hotmartchalenge/marketplace/domain/repositories/ProductRepository.java
@@ -1,8 +1,7 @@
 package com.hotmartchalenge.marketplace.domain.repositories;
 
 import com.hotmartchalenge.marketplace.domain.entities.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProductRepository extends JpaRepository<Product, Long> {}
+public interface ProductRepository extends CustomJpaRepository<Product, Long> {}
