59a6ae58de4e1820aa46058f0214b1ac50ee8199
Anderson Freitas <mfderson@gmail.com>
2020-10-04 09:47:27 -0300

    feat(api): controller
    disponibilizado endpoint para update de um produto

--- src/main/java/com/hotmartchalenge/marketplace/api/controllers/ProductController.java
+++ src/main/java/com/hotmartchalenge/marketplace/api/controllers/ProductController.java
@@ -12,6 +12,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -37,6 +38,15 @@ public class ProductController {
   @ResponseStatus(HttpStatus.CREATED)
   public ProductResDto insert(@RequestBody @Valid ProductReqDto productReqDto) {
     Product product = productReqDtoDisassembler.toEntityObject(productReqDto);
+
+    return productResDtoAssembler.toDto(productService.save(product));
+  }
+
+  @PutMapping("/{id}")
+  public ProductResDto update(
+      @PathVariable Long id, @RequestBody @Valid ProductReqDto productReqDto) {
+    Product product = productService.findById(id);
+    productReqDtoDisassembler.copyToEntityObject(productReqDto, product);
 
     return productResDtoAssembler.toDto(productService.save(product));
   }
