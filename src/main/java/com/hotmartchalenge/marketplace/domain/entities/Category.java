84d385d2d0127a7d51400f6399235ebfdcbdfe6e
Anderson Freitas <anderson.freitas@engdb.com.br>
2020-10-04 21:34:03 -0300

    feat(domain): entities
    adicionado relacionamento entre category e news

--- src/main/java/com/hotmartchalenge/marketplace/domain/entities/Category.java
+++ src/main/java/com/hotmartchalenge/marketplace/domain/entities/Category.java
@@ -1,9 +1,12 @@
 package com.hotmartchalenge.marketplace.domain.entities;
 
+import java.util.HashSet;
+import java.util.Set;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -18,4 +21,7 @@ public class Category {
   private Long id;
 
   private String name;
+
+  @OneToMany(mappedBy = "category")
+  private Set<News> news = new HashSet<>();
 }
--- src/main/java/com/hotmartchalenge/marketplace/domain/entities/News.java
+++ src/main/java/com/hotmartchalenge/marketplace/domain/entities/News.java
@@ -5,6 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -18,7 +19,7 @@ public class News {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
-  private String name;
-
   private OffsetDateTime publishedAt;
+
+  @ManyToOne private Category category;
 }
