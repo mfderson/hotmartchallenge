efe12eea0f4b58c43f21fd96744f5ef3c423855c
Anderson Freitas <anderson.freitas@engdb.com.br>
2020-10-06 09:49:00 -0300

    feat(domain): repository
    adicionada consulta para recuperar a notícia com uma data e categoria específica

--- src/main/java/com/hotmartchalenge/marketplace/domain/repositories/NewsRepository.java
+++ src/main/java/com/hotmartchalenge/marketplace/domain/repositories/NewsRepository.java
@@ -1,6 +1,24 @@
 package com.hotmartchalenge.marketplace.domain.repositories;
 
 import com.hotmartchalenge.marketplace.domain.entities.News;
+import java.time.OffsetDateTime;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
-public interface NewsRepository extends JpaRepository<News, Long> {}
+@Repository
+public interface NewsRepository extends JpaRepository<News, Long> {
+
+  @Query(
+      value =
+          "from News n where n.category.id = :categoryId AND n.publishedAt between :startDate AND :endDate")
+  public List<News> getAllBetweenDates(
+      @Param("categoryId") Long categoryId,
+      @Param("startDate") OffsetDateTime startDate,
+      @Param("endDate") OffsetDateTime endDate);
+
+  // @Query(value = "from News n where n.category.id = :categoryId")
+  // public List<News> getAllBetweenDates(@Param("categoryId") Long categoryId);
+}
