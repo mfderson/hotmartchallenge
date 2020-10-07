d12208aea67cad0256a43ffe66ecacb85b689b7a
Anderson Freitas <anderson.freitas@engdb.com.br>
2020-10-07 17:17:35 -0300

    feat(domain): repository
    implementada consulta para buscar produtos pelo nome e data atual

--- src/main/java/com/hotmartchalenge/marketplace/domain/repositories/ProductRepository.java
+++ src/main/java/com/hotmartchalenge/marketplace/domain/repositories/ProductRepository.java
@@ -1,7 +1,12 @@
 package com.hotmartchalenge.marketplace.domain.repositories;
 
 import com.hotmartchalenge.marketplace.domain.entities.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProductRepository extends CustomJpaRepository<Product, Long> {}
+public interface ProductRepository extends CustomJpaRepository<Product, Long> {
+
+  Page<Product> findAllByNameContainingIgnoreCase(String name, Pageable pageable);
+}
