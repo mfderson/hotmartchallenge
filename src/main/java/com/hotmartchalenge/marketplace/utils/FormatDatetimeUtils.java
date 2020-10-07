72d6cb3e3b70edfe7ef1e1e296f7da097cf7f560
Anderson Freitas <anderson.freitas@engdb.com.br>
2020-10-07 17:20:23 -0300

    feat(utils): format date
    tornando os metodos polimorficos, podendo receber um parametro do tipo OffsetDateTime para conversao na primeira e ultima hora do dia

--- src/main/java/com/hotmartchalenge/marketplace/utils/FormatDatetimeUtils.java
+++ src/main/java/com/hotmartchalenge/marketplace/utils/FormatDatetimeUtils.java
@@ -1,6 +1,7 @@
 package com.hotmartchalenge.marketplace.utils;
 
 import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
 
 public class FormatDatetimeUtils {
@@ -15,5 +16,38 @@ public class FormatDatetimeUtils {
     return OffsetDateTime.parse(
         String.format("%sT23:59:59+00:00", dateToBeConverted),
         DateTimeFormatter.ISO_OFFSET_DATE_TIME);
+  }
+
+  public static OffsetDateTime convertTimeToStartDay(OffsetDateTime dateToBeConverted) {
+    return OffsetDateTime.of(
+        dateToBeConverted.getYear(),
+        dateToBeConverted.getMonthValue(),
+        dateToBeConverted.getDayOfMonth(),
+        0,
+        0,
+        0,
+        0,
+        ZoneOffset.UTC);
+  }
+
+  public static OffsetDateTime convertTimeToEndDay(OffsetDateTime dateToBeConverted) {
+    return OffsetDateTime.of(
+        dateToBeConverted.getYear(),
+        dateToBeConverted.getMonthValue(),
+        dateToBeConverted.getDayOfMonth(),
+        23,
+        59,
+        59,
+        0,
+        ZoneOffset.UTC);
+  }
+
+  public static boolean compareTwoDatesOnlyDatePart(OffsetDateTime date1, OffsetDateTime date2) {
+    if (date1.getDayOfMonth() == date2.getDayOfMonth()
+        && date1.getMonth() == date2.getMonth()
+        && date1.getYear() == date2.getYear()) {
+      return true;
+    }
+    return false;
   }
 }
