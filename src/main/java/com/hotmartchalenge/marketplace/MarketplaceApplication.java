f09d097b2cdce9cf909ebcaa5c4efb0b52b470f7
Anderson Freitas <anderson.freitas@engdb.com.br>
2020-10-07 07:14:41 -0300

    feat(config): application
    Fazendo com que a aplicação trabalhe com o timezone UTC

--- src/main/java/com/hotmartchalenge/marketplace/MarketplaceApplication.java
+++ src/main/java/com/hotmartchalenge/marketplace/MarketplaceApplication.java
@@ -1,6 +1,7 @@
 package com.hotmartchalenge.marketplace;
 
 import com.hotmartchalenge.marketplace.infra.repositories.CustomJpaRepositoryImpl;
+import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -10,6 +11,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 public class MarketplaceApplication {
 
   public static void main(String[] args) {
+    TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+
     SpringApplication.run(MarketplaceApplication.class, args);
   }
 }
