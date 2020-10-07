4e202240f89da02cd38960dbc5962f63ef72b623
Anderson Freitas <anderson.freitas@engdb.com.br>
2020-10-07 17:21:44 -0300

    feat(migration): update category names
    atualizando alguns nomes de categorias por nao termos achado nenhuma noticia com os nomes antigos

---
+++ src/main/resources/db/migration/V008__update_categories_names.sql
@@ -0,0 +1,4 @@
+update category set name = 'Bitcoin' where name = 'Women''s Fashion';
+update category set name = 'Smartphone' where name = 'Men''s Fashion';
+update category set name = 'Google' where name = 'Girls'' Fashion';
+update category set name = 'Fashion' where name = 'Boys'' Fashion';
