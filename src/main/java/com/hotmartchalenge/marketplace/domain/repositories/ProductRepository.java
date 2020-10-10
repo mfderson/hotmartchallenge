9422b834a42773e260d43b491c1bd7d5c543473a
Anderson Freitas <anderson.freitas@engdb.com.br>
2020-10-10 14:30:29 -0300

    refactor(domain): repository and service
    eliminando código que não será mais utilizado

--- src/main/java/com/hotmartchalenge/marketplace/domain/repositories/ProductRepository.java
+++ src/main/java/com/hotmartchalenge/marketplace/domain/repositories/ProductRepository.java
@@ -1,18 +1,7 @@
 package com.hotmartchalenge.marketplace.domain.repositories;
 
 import com.hotmartchalenge.marketplace.domain.entities.Product;
-import java.time.OffsetDateTime;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProductRepository extends CustomJpaRepository<Product, Long> {
-
-  @Query(
-      value =
-          "select p from Product p left join p.category c left join c.news n where (lower(p.name) like '%' || :name || '%') and (n.publishedAt between :startDate and :endDate)")
-  Page<Product> findAllByNameAndDate(
-      String name, OffsetDateTime startDate, OffsetDateTime endDate, Pageable pageable);
-}
+public interface ProductRepository extends CustomJpaRepository<Product, Long> {}
