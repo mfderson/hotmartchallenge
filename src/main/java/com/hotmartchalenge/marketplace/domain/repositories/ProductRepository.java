48fdd1f1ba0043847fb3c272b7d90c7322b00280
Anderson Freitas <anderson.freitas@engdb.com.br>
2020-10-10 17:13:31 -0300

    feat(domain): repository
    adicionada consulta para retornar uma lista de restaurantes por nome

--- src/main/java/com/hotmartchalenge/marketplace/domain/repositories/ProductRepository.java
+++ src/main/java/com/hotmartchalenge/marketplace/domain/repositories/ProductRepository.java
@@ -1,7 +1,11 @@
 package com.hotmartchalenge.marketplace.domain.repositories;
 
 import com.hotmartchalenge.marketplace.domain.entities.Product;
+import java.util.List;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProductRepository extends CustomJpaRepository<Product, Long> {}
+public interface ProductRepository extends CustomJpaRepository<Product, Long> {
+
+  List<Product> findByNameContainingIgnoreCase(String name);
+}
