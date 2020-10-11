803377af5e070eee61139ea5eb822a47b1a64e4f
Anderson Freitas <anderson.freitas@engdb.com.br>
2020-10-11 17:41:12 -0300

    refactor(domain): entities
    remoçao de codigo nao mais utilizado

--- src/main/java/com/hotmartchalenge/marketplace/domain/entities/Product.java
+++ src/main/java/com/hotmartchalenge/marketplace/domain/entities/Product.java
@@ -1,8 +1,6 @@
 package com.hotmartchalenge.marketplace.domain.entities;
 
-import com.hotmartchalenge.marketplace.utils.FormatDatetimeUtils;
 import java.time.OffsetDateTime;
-import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.CascadeType;
@@ -14,8 +12,6 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
-import javax.persistence.PostLoad;
-import javax.persistence.Transient;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import org.hibernate.annotations.CreationTimestamp;
@@ -46,45 +42,4 @@ public class Product {
       fetch = FetchType.EAGER,
       orphanRemoval = true)
   private List<Sale> sales = new ArrayList<>();
-
-  @Transient private Double score;
-
-  @PostLoad
-  private void setScoreWithCalculatedValues() {
-    score =
-        calculateSalesAverageLastTwelveMonths()
-            + calculateSalesPerDayByProductCreateAt()
-            + calculateNumberOfNewsPerDay();
-  }
-
-  public Double calculateSalesAverageLastTwelveMonths() {
-    double sumRating = 0.0;
-    OffsetDateTime datetimeTwelveMonthsAgo = OffsetDateTime.now().minusMonths(12L);
-    int numberOfSales = 0;
-
-    for (Sale sale : this.getSales()) {
-      if (sale.getCreatedAt().isAfter(datetimeTwelveMonthsAgo)) {
-        numberOfSales++;
-        sumRating += sale.getRating();
-      }
-    }
-
-    return numberOfSales > 0 ? sumRating / numberOfSales : 0.0;
-  }
-
-  public Float calculateSalesPerDayByProductCreateAt() {
-    Long numberOfDays = Math.abs(ChronoUnit.DAYS.between(getCreatedAt(), OffsetDateTime.now()));
-
-    return numberOfDays > 0 ? ((float) getSales().size() / (float) numberOfDays) : 0f;
-  }
-
-  public Integer calculateNumberOfNewsPerDay() {
-    for (News news : getCategory().getNews()) {
-      if (FormatDatetimeUtils.compareTwoDatesOnlyDatePart(
-          OffsetDateTime.now(), news.getPublishedAt())) {
-        return news.getTotalResults();
-      }
-    }
-    return 0;
-  }
 }
